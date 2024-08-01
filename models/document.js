6844f15a3be968a200a208feabcbb1a4fb8d330e
MiledAmer <midou.slm8@gmail.com>
2024-08-01 10:48:43 +0100

    creating document CRUD and updating the multer middleware

--- models/document.js
+++ models/document.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const documentSchema = mongoose.Schema({
-    title: { type: String, required: true, unique: true  },
+    title: { type: String, required: true },
     description: { type: String, required: true },
     file :{ type: String, required: true },
     userID : { type: String, required: true },
     categoryID : { type: String, required: true },
     date: { type: Date, default: Date.now },
-    status: { type: String, required: true }
+    status: { type: Boolean, default: false }, 
+    courseID: { type: String }
 });
 
 
